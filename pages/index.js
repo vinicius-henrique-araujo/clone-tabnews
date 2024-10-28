74104718409de3d83d70180dcfab0aa48490e0eb
Vinicius Henrique <44846414+vinicius-henrique-araujo@users.noreply.github.com>
2024-10-28 00:56:32 +0000

    edi texto

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home(){
-    return <h1>Eu te amo  Ádila Vitoria, se vc me ama da um sorrisinho!:)  </h1>
+    return <h1>Eu te amo  Ádila Vitoria, se vc me ama da um sorrisinho! </h1>
 }
 
 export default Home; 
